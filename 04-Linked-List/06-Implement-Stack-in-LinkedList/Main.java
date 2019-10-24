bf5752b360e737abd45b5513e31c6d6c01dcfb52
crwen <1543720935@qq.com>
2019-10-24 10:16:38 +0800

    linkedList stack

---
+++ 04-Linked-List/06-Implement-Stack-in-LinkedList/Main.java
@@ -0,0 +1,24 @@
+/**
+ * ClassName: Main
+ * Description:
+ * date: 2019/10/21 20:47
+ *
+ * @author crwen
+ * @create 2019-10-21-20:47
+ * @since JDK 1.8
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		LinkedListStack<Integer> stack = new LinkedListStack<>();
+
+		for (int i = 0; i < 5; i++) {
+			stack.push(i);
+			System.out.println(stack);
+		}
+
+		stack.pop();
+		System.out.println(stack);
+	}
+
+}
