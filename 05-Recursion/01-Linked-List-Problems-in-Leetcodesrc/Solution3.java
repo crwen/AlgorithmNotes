e04bacdafab05f196280bacb193e099796280544
crwen <1543720935@qq.com>
2019-10-24 15:45:09 +0800

    Recursion

---
+++ 05-Recursion/01-Linked-List-Problems-in-Leetcodesrc/Solution3.java
@@ -0,0 +1,27 @@
+public class Solution3 {
+
+	public ListNode removeElements(ListNode head, int val) {
+		if (head == null) {
+			return null;
+		}
+		//ListNode res = removeElements(head.next, val);
+		//if (head.val == val) {
+		//	return res;
+		//} else {
+		//	head.next = res;
+		//	return head;
+		//}
+		head.next = removeElements(head.next, val);
+		return head.val == val ? head.next : head;
+	}
+
+	public static class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+
+	}
+}
