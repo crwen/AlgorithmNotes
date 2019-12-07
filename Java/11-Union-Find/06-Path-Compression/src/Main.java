6f3be715ffad5c6ed7df144b46e8dd36f3a1533d
crwen <1543720935@qq.com>
2019-12-07 16:41:06 +0800

    path compression

---
+++ Java/11-Union-Find/06-Path-Compression/src/Main.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+
+/**
+ * ClassName: Main
+ * Description:
+ * date: 2019/12/7 14:52
+ *
+ * @author crwen
+ * @create 2019-12-07-14:52
+ * @since JDK 1.8
+ */
+public class Main {
+
+	private static double testUF(UF uf, int m) {
+
+		int size = uf.getSize();
+		Random random = new Random();
+
+		long startTime = System.nanoTime();
+
+		for (int i = 0; i < m; i++) {
+			int a = random.nextInt(size);
+			int b = random.nextInt(size);
+			uf.unionElements(a, b);
+		}
+
+		for (int i = 0; i < m; i++) {
+			int a = random.nextInt(size);
+			int b = random.nextInt(size);
+			uf.isConnection(a, b);
+		}
+
+		long endTime = System.nanoTime();
+
+		return (endTime - startTime) / 1e9;
+	}
+
+	public static void main(String[] args) {
+		int size = 10000000;
+		int m = 10000000;
+
+		UnionFind1 uf1 = new UnionFind1(size);
+		UnionFind2 uf2 = new UnionFind2(size);
+		UnionFind3 uf3 = new UnionFind3(size);
+		UnionFind4 uf4 = new UnionFind4(size);
+		UnionFind5 uf5 = new UnionFind5(size);
+
+		//System.out.println("UnionFind1 : " + testUF(uf1, m) + " s");
+		//System.out.println("UnionFind2 : " + testUF(uf2, m) + " s");
+		System.out.println("UnionFind3 : " + testUF(uf3, m) + " s");
+		System.out.println("UnionFind4 : " + testUF(uf4, m) + " s");
+		System.out.println("UnionFind5 : " + testUF(uf5, m) + " s");
+	}
+}
