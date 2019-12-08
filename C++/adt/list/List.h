ef507230459890d7edba4d245be73070f6f4e548
crwen <1543720935@qq.com>
2019-12-08 19:17:56 +0800

    add list interface

---
+++ C++/adt/list/List.h
@@ -0,0 +1,22 @@
+//
+// Created by crwen on 2019/12/8.
+//
+
+#ifndef DATASTRUCTURE_LIST_H
+#define DATASTRUCTURE_LIST_H
+
+template <class T>
+class List {
+public:
+    virtual int getSize() = 0;
+
+    virtual bool isEmpty() = 0;
+
+    virtual T get(int index);
+
+    virtual void add(T e) = 0;
+
+    virtual T remove(int index);
+};
+
+#endif //DATASTRUCTURE_LIST_H
