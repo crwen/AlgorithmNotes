c2f0a7f285cd8994964c96ac7d4c1688e1dfe0f2
crwenAyu <1543720935@qq.com>
2020-03-14 16:52:31 +0800

    104 maximum depth of binary tree

---
+++ Leetcode-Top100/Maximum-Depth-Of-Binary-Tree/Java/src/Solution.java
@@ -0,0 +1,27 @@
+package maximumdepthofbinarytree;
+
+/**
+ * ClassName: Solution
+ * Description:
+ * date: 2020/3/14 16:50
+ *
+ * @author crwen
+ * @create 2020-03-14-16:50
+ * @since JDK 1.8
+ */
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+
+public class Solution {
+    public int maxDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+}
