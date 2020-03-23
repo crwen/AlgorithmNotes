b878f3fd74c3177840e728d403cce2f2501d8c9a
crwenAyu <1543720935@qq.com>
2020-03-23 17:59:51 +0800

    minimum window substring

---
+++ Leetcode-Top100/Minimum-Window-Substring/Java/src/Solution.java
@@ -0,0 +1,58 @@
+
+import java.util.HashMap;
+
+/**
+ * ClassName: Solution
+ * Description:
+ * date: 2020/3/23 16:36
+ *
+ * @author crwen
+ * @create 2020-03-23-16:36
+ * @since JDK 1.8
+ */
+public class Solution {
+    public String minWindow(String s, String t) {
+        if (s == null || s.length() < t.length() || s.length() == 0) {
+            return "";
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+        for (char c : t.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+        int left = 0;
+        int minLeft = 0;
+        int minLen = s.length() + 1;
+        int count = 0;
+        for (int right = 0; right < s.length(); right ++) {
+            char ch = s.charAt(right);
+            if (map.containsKey(ch)) {
+                map.put(ch, map.get(ch) - 1);
+                if (map.get(ch) >= 0) {
+                    count ++;
+                }
+
+                while(count == t.length()){
+                    if(right - left + 1 < minLen){
+                        minLeft = left;
+                        minLen = right - left + 1;
+                    }
+                    if(map.containsKey(s.charAt(left))){
+                        map.put(s.charAt(left),map.get(s.charAt(left))+1);
+                        if(map.get(s.charAt(left)) > 0){
+                            count --;
+                        }
+                    }
+                    left ++ ;
+                }
+            }
+        }
+        if (minLen > s.length()) {
+            return "";
+        }
+        return s.substring(minLeft, minLeft + minLen);
+    }
+}
